e03b44646d09bc36ebf40415417914631b1f622d
bassirou ngom <bassiroungom@esp.sn>
2017-04-17 21:15:49 +0000

    update turbidity

--- TurbidityMeter/TurbidityMeter.ino
+++ TurbidityMeter/TurbidityMeter.ino
@@ -13,10 +13,10 @@ void loop() {
   int sensorValue = analogRead(A2);// read the input on analog pin 0:
   float voltage = sensorValue * (5.0 / 1023.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float value = 150.0*voltage - 80;
-   Serial.print(voltage);
-   Serial.print(" V => ");
-  Serial.print(value); // print out the value you read:
-   Serial.println(" NTU");
+   //Serial.print(voltage);
+   //Serial.print(" V => ");
+  Serial.println(value); // print out the value you read:
+   //Serial.println(" NTU");
   delay(500);
 }
 
