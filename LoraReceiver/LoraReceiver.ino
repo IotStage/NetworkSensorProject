5f4e645785f728ac33b6def76ad90923f91a4b91
bassirou ngom <bassiroungom@esp.sn>
2017-04-25 12:29:18 +0000

    receiver client

--- LoraReceiver/LoraReceiver.ino
+++ LoraReceiver/LoraReceiver.ino
@@ -1,27 +1,4 @@
-/*
- *  Semtech SX1272 module managing with Arduino
- *
- *  Copyright (C) 2014 Libelium Comunicaciones Distribuidas S.L.
- *  http://www.libelium.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 2.1 of the License, or
- *  (at your option) any later version.
 
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
-
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Version:		1.0
- *  Design:		David Gascón
- *  Implementation:	Covadonga Albiñana
- */
- 
 // Include the SX1272 and SPI library: 
 #include "SX1276.h"
 #include <SPI.h>
