70e21d5b12612ad7eb031e2f1a13d27343bce09b
bassirou ngom <bassiroungom@esp.sn>
2017-04-25 12:28:24 +0000

    vent sensor

--- anemometre/anemometre.ino
+++ anemometre/anemometre.ino
@@ -1,13 +1,15 @@
 void setup() {
 Serial.begin(9600);
-pinMode(2, INPUT);
+pinMode(5, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int val = digitalRead(2);
-Serial.println(val);
-//Serial.println(" V");
+  int val = digitalRead(5);
+   float val2 = analogRead(A0)*(5.0/1023.0);
+Serial.print(val*5);
+Serial.print(", ");
+Serial.println(val2);
 delay(5);
 
 
