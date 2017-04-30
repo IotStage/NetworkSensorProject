9fc7de37225d489ad262b8974ea23a1a158b11d5
bassirou ngom <bassiroungom@esp.sn>
2017-04-30 03:33:08 +0000

    pluvio

--- pluviometre/pluviometre.ino
+++ pluviometre/pluviometre.ino
@@ -1,28 +1,43 @@
-const int pinPluvio = 3;
-int nbBasculement=0;
-int avant=1;
-int value=0;
+#include "Timer.h"
+
+Timer t;
+const int pinPluvio = 8;
+int nbBasculement = 0;
+int avant = 1;
+int value = 0;
+long pause=0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(pinPluvio, INPUT);
+  t.every(5, getBasculement);
+  pause = millis();
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  getBasculement();
-  Serial.println(nbBasculement);
+  ;
+  if(millis() - pause > 5000)
+  {
+    Serial.println(nbBasculement);//delay(500);
+    nbBasculement=0;
+    pause = millis();
+  }
+  t.update();
+  delay(10);
+  
 }
 
-void getBasculement(){
-   value = digitalRead(pinPluvio);
+void getBasculement() {
+  //nbBasculement=0;
+  value = digitalRead(pinPluvio);
   //tension_pluvio=(sensorValue_pluvio*5)/1023;
-  Serial.print(value);
-  Serial.print(",");
-  if(value==0 && avant==1){
-    nbBasculement++;
-  }
-  avant=value;
-  //return nbtour;
+  //Serial.print(value);
+  //Serial.print(",");
+  if (value == 0 && avant == 1) {
+    nbBasculement++;}
+  avant = value;
+  //return nbBasculement;
 }
 
