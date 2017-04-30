7347692c8f32259ecf7062e9a21db405dd1daa79
bassirou ngom <bassiroungom@esp.sn>
2017-04-30 03:31:43 +0000

    code capteur vent

--- anemometre/anemometre.ino
+++ anemometre/anemometre.ino
@@ -6,10 +6,10 @@ pinMode(5, INPUT);
 void loop() {
   // put your main code here, to run repeatedly:
   int val = digitalRead(5);
-   float val2 = analogRead(A0)*(5.0/1023.0);
-Serial.print(val*5);
-Serial.print(", ");
-Serial.println(val2);
+   //float val2 = analogRead(A0)*(5.0/1023.0);
+Serial.println(val);
+//Serial.print(", ");
+//Serial.println(val2);
 delay(5);
 
 
