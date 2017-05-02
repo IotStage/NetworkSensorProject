fe9f56499cfa56979066c61ef37a99a6b8a54453
bassirou ngom <bassiroungom@esp.sn>
2017-05-02 18:06:28 +0000

    anémomètre

--- anemometre/anemometre.ino
+++ anemometre/anemometre.ino
@@ -1,11 +1,11 @@
 void setup() {
 Serial.begin(9600);
-pinMode(5, INPUT);
+pinMode(3, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int val = digitalRead(5);
+  int val = digitalRead(3);
    //float val2 = analogRead(A0)*(5.0/1023.0);
 Serial.println(val);
 //Serial.print(", ");
