0a6a0d2b588afd36e16b6379d960d9c147111f99
bassirou ngom <bassiroungom@esp.sn>
2017-05-05 17:43:17 +0000

    organisation

--- GateWay/EthernetMaster/EthernetMaster.ino
+++ GateWay/EthernetMaster/EthernetMaster.ino
@@ -2,6 +2,17 @@
 #include <Ethernet.h>
 #include <Wire.h>
 
+
+
+// parametres shield ethernet
+//IPAddress ip(172,16,0,101);
+IPAddress ip(10,2,1,11);
+EthernetClient client;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+char server [] = "10.2.1.10";
+
+int taille=2;
+
 #define SLAVE_RF 5
 #define SLAVE_SMS 3
 //#define TAILLERF 25
@@ -9,7 +20,8 @@
 
 
 void setup() {
- Serial.begin();
+ Serial.begin(9600);
+ Wire.begin();
 
 }
 
