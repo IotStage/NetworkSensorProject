d9144d98e72b247d04bb54d896902dad37efa223
bassirou ngom <bassiroungom@esp.sn>
2017-05-10 15:44:37 +0000

    RFCC1101 sender and receiver

---
+++ Communications/RFCC1101Sender/RFCC1101Sender.ino
@@ -0,0 +1,80 @@
+#include "EEPROM.h"
+#include "cc1101.h"
+//creation d'un objet cc1101
+CC1101 cc1101;
+
+byte syncWord[2] = {199, 0}; // mot de synchronisation
+
+CCPACKET paquet; // le paquet envoye (debut de trame | syncword | donnees utiles | FCS)
+
+// a flag that a wireless packet has been received
+boolean packetAvailable = false;
+int i=0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+    Serial.begin(9600);
+  
+  //lampe
+ // pinMode(LAMPE, OUTPUT);
+  
+  // initialisation de l'antenne RF
+  cc1101.init();
+  cc1101.set_433_GFSK_500_K();        //changement du type de modulation et du debit (modulation GFSK, debit 1,2 kbauds avec frequence 433 Mhz)
+   cc1101.setChannel(10);
+  cc1101.disableAddressCheck(); //if not specified, will only display "packet received"
+  attachInterrupt(0, cc1101signalsInterrupt, FALLING);
+  Serial.println("Initialisarion antenne RF terminee...");
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String bjr="bonjour tout le monde, ceci est mon ";
+  bjr+=String(i);
+  bjr+="em bonjour";
+  i++;
+  formatPaquet(bjr);
+  delay(1000);
+}
+
+/* Handle interrupt from CC1101 (INT0) gdo0 on pin2 */
+void cc1101signalsInterrupt(void){
+  // set the flag that a package is available
+  packetAvailable = true;
+}
+
+
+void envoiPaquet() {
+  if(cc1101.sendData(paquet)){
+    Serial.println("Envoi termine");
+   }else{
+    Serial.println("Echec de l'envoi");
+  }
+}
+
+/***
+ * format segmentatoin des donnees a envoyer en paquet, 
+ * ajout d'une entete specifique (~@]#) pour limiter les paquets
+ */
+void formatPaquet(String message){
+  Serial.print("message :");
+  Serial.println(message);
+  if(message.length()<61){
+    Serial.print("Taille ");
+    Serial.println(message.length());
+    paquet.length=message.length();
+    message.getBytes(paquet.data, message.length()+1);
+    envoiPaquet();
+  }
+  else{
+    String partie1 = message.substring(0, 55);
+    String partie2 = message.substring(55);
+    partie1+="~@]#`";
+    formatPaquet(partie1);
+    //delay(500);
+    formatPaquet(partie2);
+  }
+}
+
